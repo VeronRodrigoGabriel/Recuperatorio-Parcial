3d4dd3c5bd321d824c7c429a284cecc0200bab19
Rodri.V <veronrodrigo98@gmail.com>
2023-07-05 14:29:16 -0300

    Se agrego el modelo de datos

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,74 @@
 // TODO: Crear modelo de datos de Reserva
+const { DataTypes, sequelize } = require('../db');
+
+const Reserva = sequelize.define('Reserva', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    codigo_reserva: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+        defaultValue: new Date().getTime()
+    },
+    nombre: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    apellido: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    pais: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    ciudad: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    fecha_vuelo: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    telefono: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    estado: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: 'reservas'
+});
+
+
+Reserva.sync({ force: false }).then(() => {
+    console.log('Tabla de Reservas creada');
+});
+
+module.exports = Reserva;
